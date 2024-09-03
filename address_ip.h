30d5f16232c46908f778ad4d054b2b98aab9f6c4
Ainalawrena <167605889+Ainalawrena@users.noreply.github.com>
2024-09-03 18:31:33 +0200

    Add files via upload

---
+++ address_ip.h
@@ -0,0 +1,8 @@
+void enter_msr(char *chaine1);
+void separe(char *chaine,int *a,int *b,int *c,int *d);
+void decimal(int x,char *chaine);
+void condition(int *total,int a1,int b1,int c1,int d1,char *chaine1,char *chaine2,char *chaine3,char *chaine4);
+void execution_msr(char *chaine,char *ip,int a,int b,int c,int d);
+int tableau(int *tab);
+void valide();
+void board(int *total,int *boardcast);
